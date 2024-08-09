115bb335f8b15d8eb90e24873d910caffe2a7a28
DHPR <133486266+Deepking88@users.noreply.github.com>
2024-08-09 15:29:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/masoombalak88/DKPV",
+    "https://github.com/Deepking88/Kingdk",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
